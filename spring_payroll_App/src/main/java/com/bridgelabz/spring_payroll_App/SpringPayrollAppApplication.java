f64cb5f42601f7f0dfd8421fa63f901804c60e2d
Arpita030 <0111as211013.arpita@gmail.com>
2025-02-24 15:21:52 +0530

    [Arpita]add. Creating spring project

---
+++ spring_payroll_App/src/main/java/com/bridgelabz/spring_payroll_App/SpringPayrollAppApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.spring_payroll_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPayrollAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPayrollAppApplication.class, args);
+	}
+
+}
---
+++ spring_payroll_App/src/test/java/com/bridgelabz/spring_payroll_App/SpringPayrollAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.spring_payroll_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPayrollAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
