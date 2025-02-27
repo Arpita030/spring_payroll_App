aec97df93ef54167d76e20cca546a7303b25017c
Arpita030 <0111as211013.arpita@gmail.com>
2025-02-27 11:19:20 +0530

    [Arpita]add. Using slf4j annotation for logging

--- spring_payroll_App/src/main/java/com/bridgelabz/spring_payroll_App/EmployeePayrollAppApplication.java
+++ spring_payroll_App/src/main/java/com/bridgelabz/spring_payroll_App/EmployeePayrollAppApplication.java
@@ -3,12 +3,14 @@ package com.bridgelabz.spring_payroll_App;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import lombok.extern.slf4j.Slf4j;
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeePayrollAppApplication.class, args);
+		log.info("Employee payroll App started");
 	}
 
 }
